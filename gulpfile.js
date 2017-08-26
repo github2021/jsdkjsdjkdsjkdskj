1a84b9998ea6339f822eb1f0b1df1bfd32396464
K J Norton <rhchristian1@gmail.com>
2017-08-26 15:50:54 +0100

    annnnnnnnnnnnnnnnalllll

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,18 @@ var notify = require('gulp-notify');
 // Development tasks
 // --------------------------------------------------------------
 
+
+
+
+gulp.task('travis', ['build', 'testServerJS']. function() {
+
+    process.exit(0);
+    
+});
+
+
+
+
 // Live reload business.
 gulp.task('reload', function () {
     livereload.reload();
